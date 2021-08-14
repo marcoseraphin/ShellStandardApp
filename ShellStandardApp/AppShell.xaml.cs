456f12ee0a69bb2fc9f8563c636033f703ddc96d
Marco Seraphin <seraphin@develope-it.de>
2021-08-14 11:57:09 +0200

    xxx

--- ShellStandardApp/AppShell.xaml.cs
+++ ShellStandardApp/AppShell.xaml.cs
@@ -25,7 +25,7 @@ namespace ShellStandardApp
 
 			MessagingCenter.Subscribe<StartPage, string>(this, "ChangeLanguage", async (sender, arg) =>
 			{
-				StartTab.Title = "123XXXXX";
+				StartTab.Title = "1234XXXXX";
 			});
 
 		}
